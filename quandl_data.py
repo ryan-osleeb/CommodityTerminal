4769f6f3609baa319f9f06801e09cb63f8281e76
ryan-osleeb <59590885+ryan-osleeb@users.noreply.github.com>
2021-03-01 21:58:14 -0500

    Add files via upload
    more technical analysis code

--- quandl_data.py
+++ quandl_data.py
@@ -2,17 +2,17 @@ import quandl
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from technical_analysis import macd, moving_avgs, rsi
+from technical_analysis import macd, moving_avgs, rsi, stoch, bollinger_band, technical_summary
 
-COMMODITY = {"crude oil": "EIA/PET_RWTC_D"}
+COMMODITY = {"crude oil": "CHRIS/CME_CL1"}
 
-def get_price(commodity, start, end):
-	data = quandl.get(COMMODITY[commodity], start_date = start, end_date = end)
+def get_price(commodity, start):
+	data = quandl.get(COMMODITY[commodity], start_date = start)
 	data = pd.DataFrame(data)
 	data['Date'] = data.index
 	return data
 
 if __name__ == "__main__":
-	comm = get_price("crude oil", "2019-01-01", "2021-02-10")
+	comm = get_price("crude oil", "2020-07-01")
 	#print(comm)
-	rsi(comm)
+	technical_summary(comm)
--- technical_analysis.py
+++ technical_analysis.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 
 def macd(df):
-	exp1 = df.Value.ewm(span = 12, adjust = False).mean()
-	exp2 = df.Value.ewm(span = 26, adjust = False).mean()
+	exp1 = df.Settle.ewm(span = 12, adjust = False).mean()
+	exp2 = df.Settle.ewm(span = 26, adjust = False).mean()
 	macd = exp1 - exp2
 	exp3 = macd.ewm(span = 9, adjust = False).mean()
 	pd.plotting.register_matplotlib_converters()
@@ -13,21 +13,19 @@ def macd(df):
 	plt.plot(df.Date, exp3)
 	plt.show()
 
-
 def moving_avgs(df, days):
-	rolling = df.Value.rolling(window = days).mean()
-	rolling = rolling.replace(np.nan, np.mean(df.Value))
+	sma = df.Settle.rolling(window = days).mean()
+	sma = sma.replace(np.nan, np.mean(df.Settle))
 	pd.plotting.register_matplotlib_converters()
-	plt.plot(df.Date, df.Value)
-	plt.plot(df.Date, rolling)
+	plt.plot(df.Date, df.Settle)
+	plt.plot(df.Date, sma)
 	plt.show()
-
 
 def rsi(df):
 	gains = []
 	losses = []
-	for prices in range(len(df.Value)-1):
-		change = df.Value[prices+1] - df.Value[prices]
+	for prices in range(len(df.Settle)-1):
+		change = df.Settle[prices+1] - df.Settle[prices]
 		if change > 0:
 			gains.append(change)
 			losses.append(0)
@@ -41,11 +39,86 @@ def rsi(df):
 		rs = (avg_gain/avg_loss)
 		rsi = (100-(100/(1+rs)))
 		RSI.append(rsi)
+	oversold = [30] * len(RSI)
+	overbought = [70] * len(RSI)
 	pd.plotting.register_matplotlib_converters()
 	plt.plot(df.Date[15:], RSI)
+	plt.plot(df.Date[15:], oversold)
+	plt.plot(df.Date[15:], overbought)
 	plt.show()
 
-#def stoch(df):
+def stoch(df):
+	low = df.Settle.rolling(window = 14).min()
+	high = df.Settle.rolling(window = 14).max()
+	K = 100*((df.Settle-low)/(high-low))
+	D = K.rolling(window = 3).mean()
+	pd.plotting.register_matplotlib_converters()
+	plt.plot(df.Date, K)
+	plt.plot(df.Date, D)
+	plt.show()
+
+def bollinger_band(df):
+	avg = df.Settle.rolling(window = 20).mean()
+	std = df.Settle.rolling(window = 20).std()
+	upper = avg + (2 * std)
+	lower = avg - (2 * std)
+	pd.plotting.register_matplotlib_converters()
+	plt.plot(df.Date, df.Settle)
+	plt.plot(df.Date, avg)
+	plt.plot(df.Date, upper)
+	plt.plot(df.Date, lower)
+	plt.show()
 
 
+def technical_summary(df):
+	#bollinger bands
+	avg = df.Settle.rolling(window = 20).mean()
+	std = df.Settle.rolling(window = 20).std()
+	upper = avg + (2 * std)
+	lower = avg - (2 * std)
 
+	#rsi
+	gains = []
+	losses = []
+	for prices in range(len(df.Settle)-1):
+		change = df.Settle[prices+1] - df.Settle[prices]
+		if change > 0:
+			gains.append(change)
+			losses.append(0)
+		else:
+			losses.append(abs(change))
+			gains.append(0)
+	RSI = []
+	for i in range(len(gains)-14):
+		avg_gain = np.mean(gains[i:i+14])
+		avg_loss = np.mean(losses[i:i+14])
+		rs = (avg_gain/avg_loss)
+		rsi = (100-(100/(1+rs)))
+		RSI.append(rsi)
+	oversold = [30] * len(RSI)
+	overbought = [70] * len(RSI)
+
+	#macd
+	exp1 = df.Settle.ewm(span = 12, adjust = False).mean()
+	exp2 = df.Settle.ewm(span = 26, adjust = False).mean()
+	macd = exp1 - exp2
+	exp3 = macd.ewm(span = 9, adjust = False).mean()
+
+	pd.plotting.register_matplotlib_converters()
+	fig, axs = plt.subplots(3)
+	fig.suptitle('Technical Analysis')
+	axs[0].plot(df.Date, df.Settle)
+	axs[0].plot(df.Date, upper)
+	axs[0].plot(df.Date, avg)
+	axs[0].plot(df.Date, lower)
+	axs[0].set_title('Bollinger Bands')
+	axs[1].plot(df.Date[15:], RSI)
+	axs[1].plot(df.Date[15:], oversold)
+	axs[1].plot(df.Date[15:], overbought)
+	axs[1].set_title('RSI')
+	axs[2].plot(df.Date, macd)
+	axs[2].plot(df.Date, exp3)
+	axs[2].set_title('MACD')
+	for ax in axs:
+		ax.label_outer()
+	plt.show()
